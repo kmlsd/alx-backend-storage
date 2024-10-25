1daeb235bc7cea000a0e7755d64a1da60aec49d1
kmlsd <kmlsd4757@gmail.com>
2024-10-25 11:25:34 -0700

    11-schools_by_topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env python3
+"""
+find by topic
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Find by topic
+    """
+    return mongo_collection.find({"topics": topic})
+
